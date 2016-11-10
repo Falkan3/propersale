dfc158e7d8da0ea4472eb8c99997dd35d8280cae
Adam Kocić <adam.kocic@automatkredytowy.pl>
2016-11-10 13:13:59 +0100

    logo

--- resources/views/main/header.blade.php
+++ resources/views/main/header.blade.php
@@ -10,7 +10,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#"><img id="logo" src="{{ URL::asset('images/logo-jasne.png') }}"/></a>
+                <a class="navbar-brand" href="#"><img id="logo" src="{{ URL::asset('images/logo-n-jasne.png') }}"/></a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
